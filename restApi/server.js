29ab019896f1d66580d2b6fb8cd513e028bbf013
hernan <hernanm992@gmail.com>
2014-10-27 21:32:27 -0300

    si no hay datos cargados para estadisticas, que te muestre un mensaje

--- restApi/server.js
+++ restApi/server.js
@@ -94,15 +94,19 @@ router.route('/:param1/estadisticas')
 		    res.writeHead(200, {"Content-Type": "text/html"});
 		    res.write(replies.length + " ips:" + "</BR>");
 		    client.scard("ips", function (err, total){//me traigo la cantidad de elementos de la lista
-			    replies.forEach(function (reply, i) {//recorro los elementos de la lista
-			    	client.get(reply, function (err, cant){
-	                    res.write(reply + " : " + cant + "</BR>");
-	                    if(parseInt(total) == (parseInt(i) + 1)){
-	                    	res.end();
-	                    }
-			        	//console.log(" sorted --> " + i + ": " + reply + " : " + cant);
-			    	});
-			    });    
+		    	if(parseInt(total) == 0){
+		    		res.json({ message: 'No hay estadisticas para mostrar' });
+		    	}else{	
+				    replies.forEach(function (reply, i) {//recorro los elementos de la lista
+				    	client.get(reply, function (err, cant){
+		                    res.write(reply + " : " + cant + "</BR>");
+		                    if(parseInt(total) == (parseInt(i) + 1)){
+		                    	res.end();
+		                    }
+				        	//console.log(" sorted --> " + i + ": " + reply + " : " + cant);
+				    	});
+				    });    
+		    	}
 		    });
 		});
 		//res.json({ message: 'Voy a mostrar estadisticas' });
