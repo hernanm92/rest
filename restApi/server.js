cf0b3664be251ce6c5bcb1190a5195e0a119940b
hernan <hernanm992@gmail.com>
2014-10-27 21:16:58 -0300

    me traigo la cantidad de elementos de la lista, para saber cuando dejar de imprimir por pantalla

--- restApi/server.js
+++ restApi/server.js
@@ -86,20 +86,24 @@ router.get('/', function(req, res) {
 // ----------------------------------------------------
 router.route('/:param1/estadisticas')
 	.get(function(req, res) {
-		client.sort("ips","by","*","desc", function (err, replies) {
+		client.sort("ips","by","*","desc", function (err, replies) {//me traigo la lista ordenada
 		    if (err) {
 		        return console.error("error response - " + err);
 		    }
 
 		    res.writeHead(200, {"Content-Type": "text/html"});
 		    res.write(replies.length + " replies:");
-		    replies.forEach(function (reply, i) {
-		    	client.get(reply, function (err, cant){
-                    res.write(" sorted --> " + i + ": " + reply + " : " + cant);
-		        	//console.log(" sorted --> " + i + ": " + reply + " : " + cant);
-		    	});
+		    client.scard("ips", function (err, total){//me traigo la cantidad de elementos de la lista
+			    replies.forEach(function (reply, i) {//recorro los elementos de la lista
+			    	client.get(reply, function (err, cant){
+	                    res.write(" sorted --> " + i + ": " + reply + " : " + cant);
+	                    if(parseInt(total) == (parseInt(i) + 1)){
+	                    	res.end();
+	                    }
+			        	//console.log(" sorted --> " + i + ": " + reply + " : " + cant);
+			    	});
+			    });    
 		    });
-		    res.end();
 		});
 		//res.json({ message: 'Voy a mostrar estadisticas' });
 	});
